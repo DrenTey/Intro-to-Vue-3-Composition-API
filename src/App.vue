6dd27be3d2eb20df42ee2cde6c02c80997c59082
DrenTey <stemicha@gmail.com>
2026-06-24 23:34:49 +0200

    Adding code solution, with double ref

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 const product = ref('Socks')
 const image = ref(socksBlueImage)
+const url1 = ref('https://google.com')
+const url2t = ref("Link2")
+const url = ref(url1)
+// Add a url ref
+//Bind the url to an anchor tag’s href attribute
 </script>
 
 <template>
@@ -11,6 +16,7 @@ const image = ref(socksBlueImage)
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
+        <a :href="url"> {{ url2t }}</a>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
