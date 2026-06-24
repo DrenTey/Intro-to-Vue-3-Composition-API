e59a3ae4a278b2a4cd53885c9b1e05162b4cbfaf
DrenTey <stemicha@gmail.com>
2026-06-24 23:27:41 +0200

    End of lesson, before code challenge

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { ref } from 'vue'
-
+import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 const product = ref('Socks')
-
+const image = ref(socksBlueImage)
 </script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img v-bind:src="image">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
