9cb6c53bf3b1cb5a8b98c65215228fd4d76b2d20
DrenTey <stemicha@gmail.com>
2026-06-24 23:40:07 +0200

    Simplifying to one url, like in the course

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 const product = ref('Socks')
 const image = ref(socksBlueImage)
-const url1 = ref('https://google.com')
-const url2t = ref("Link2")
-const url = ref(url1)
+const url = ref('https://drentey.github.io')
+// const url1 = ref('https://google.com')
+//const url2t = ref("Link2")
+//const url = ref(url1)
 // Add a url ref
 //Bind the url to an anchor tag’s href attribute
 </script>
@@ -16,7 +17,7 @@ const url = ref(url1)
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
-        <a :href="url"> {{ url2t }}</a>
+        <a :href="url"> DT link</a>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
