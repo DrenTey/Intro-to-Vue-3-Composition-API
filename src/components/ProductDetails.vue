d123bbce3a36d060d5b848f3c72089aaefd2f635
DrenTey <stemicha@gmail.com>
2026-06-26 17:41:06 +0200

    Doing codeChallenge

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+
+const props = defineProps({
+    details: {
+        type: Array,
+        required: true
+    }
+})
+</script>
+
+<template>
+
+</template>
